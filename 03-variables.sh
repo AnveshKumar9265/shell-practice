ecc2b2cc1f61cfe2a402e4f23e899c332a62d87c
AnveshKumar9265 <dkanveshkumar@gmail.com>
2026-05-09 17:31:33 +0530

    Updated new File

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="Trump"
+PERSON2="Obama"
+
+echo "$PERSON1 : Hi Hello $PERSON2 "
+echo "$PERSON2 : Hi Hello  $PERSON1"
+echo "$PERSON1 : How are you $PERSON2?"
+echo "$PERSON2 : I am fine $PERSON1, How are you ?"
