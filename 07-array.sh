64acd9f4efe52a18e32daf9b5fe4390fa5a568fb
AnveshKumar9265 <dkanveshkumar@gmail.com>
2026-05-09 18:24:48 +0530

    Updated new File

---
+++ 07-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("TRILL" "THE DARK KNIGHT" "INCEPTION"  "INTERSTELLAR")
+
+echo "my favourite movies are :: ${MOVIES[@]} "
+
+echo "my first movie is :: ${MOVIES[0]} "
+echo "my second movie is :: ${MOVIES[1]} "
